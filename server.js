7e9433d5e9818248d486b85b8841fe24902b2f8a
athena272 <guilhermera272@gmail.com>
2024-11-09 11:12:06 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import url from 'url'
+import path from 'path'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+const currentPath = url.fileURLToPath(import.meta.url)
+const publicDirectory = path.join(currentPath, "../public")
+
+app.use(express.static(publicDirectory))
+app.listen(PORT, () => console.log("Server on 🔥"))

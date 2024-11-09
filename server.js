1cf69622494595ae62e29aecf30ccf1ddb0d95ed
athena272 <guilhermera272@gmail.com>
2024-11-09 11:35:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import url from 'url'
 import path from 'path'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 const currentPath = url.fileURLToPath(import.meta.url)
 const publicDirectory = path.join(currentPath, "../public")
-
 app.use(express.static(publicDirectory))
-app.listen(PORT, () => console.log("Server on 🔥"))
+
+const httpServer = http.createServer(app)
+httpServer.listen(PORT, () => console.log("Server on 🔥"))
+
+const socket_io = new Server(httpServer) 
+
+socket_io.on("connection", () => console.log("Connection established 👨‍💻"))

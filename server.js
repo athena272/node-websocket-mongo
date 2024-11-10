9e7b5d806a906222580e4ee512b863d15429b623
athena272 <guilhermera272@gmail.com>
2024-11-10 01:15:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(express.static(publicDirectory))
 const httpServer = http.createServer(app)
 httpServer.listen(PORT, () => console.log("Server on 🔥"))
 
-const io = new Server(httpServer) 
-
-export default io
+export const socket_io = new Server(httpServer) 
 // socket_io.on("connection", (socket) => console.log(`Connection established 👨‍💻 id:${socket.id}`))
 

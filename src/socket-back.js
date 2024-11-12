b4083c131fb6f622719286ee900a8cc0b51b1ff1
athena272 <guilhermera272@gmail.com>
2024-11-12 19:46:26 -0300

    Update socket-back.js

--- src/socket-back.js
+++ src/socket-back.js
@@ -4,6 +4,10 @@ io.on("connection", (socket) => {
     console.log(`Connection established 🚀 id: ${socket.id}`)
 
     //Server side
+    socket.on("select_room", (roomName) => {
+        socket.join(roomName)
+    })
+
     socket.on("text_editor", (text) => {
         // io.emit("all_clients_text", text) MANDARIA PARA TODOS, INCLUSIVE ELA MESMO
         socket.broadcast.emit("send_text_all_clients", text)

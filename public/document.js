d27719ce8219a37c7ffe08e8d3dca9ef3e4c6c57
athena272 <guilhermera272@gmail.com>
2024-11-12 19:16:53 -0300

    Update document.js

--- public/document.js
+++ public/document.js
@@ -1,6 +1,11 @@
 import { emitTextEditor } from "./socket-front-document.js"
 
+const urlParams = new URLSearchParams(window.location.search)
+const roomName = urlParams.get('roomName')
+const documentTitle = document.getElementById('titulo-documento')
 const textEditor = document.getElementById('editor-texto')
+
+documentTitle.textContent = roomName || 'Documento sem título'
 
 export function updateTextEditor(text) {
     textEditor.value = text

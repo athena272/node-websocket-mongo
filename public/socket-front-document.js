4397ab8cf4f5ceb8c8e98e1b7708c6aad103eccc
athena272 <guilhermera272@gmail.com>
2024-11-13 02:55:24 -0300

    Update socket-front-document.js

--- public/socket-front-document.js
+++ public/socket-front-document.js
@@ -6,11 +6,11 @@ export function selectRoom(roomName) {
     socket.emit('select_room', roomName)
 }
 
-export function emitTextEditor(text) {
-    socket.emit("text_editor", text)
+export function emitTextEditor({ text, roomName }) {
+    socket.emit("text_editor", { text, roomName })
 }
 
-socket.on("send_text_all_clients", (text) => updateTextEditor(text))
+socket.on("send_text_clients", (text) => updateTextEditor(text))
 
 socket.on("disconnect", (motive) => {
     console.log(`Servidor desconectado 💀!\nMotivo: ${motive}`);

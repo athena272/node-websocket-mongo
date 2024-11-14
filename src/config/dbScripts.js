ab0e29ec64a83955f71e9e72cc9801f9b2135e73
athena272 <guilhermera272@gmail.com>
2024-11-14 03:30:32 -0300

    Update dbScripts.js

--- src/config/dbScripts.js
+++ src/config/dbScripts.js
@@ -4,3 +4,18 @@ export async function findRoom(roomName) {
     const room = await roomsCollection.findOne({ name: roomName })
     return room
 }
+
+export async function updateRoom({ roomName, text }) {
+    const roomUpdate = await roomsCollection.updateOne(
+        {
+            name: roomName,
+        },
+        {
+            $set: {
+                text
+            }
+        }
+    )
+
+    return roomUpdate
+}

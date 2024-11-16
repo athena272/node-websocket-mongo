acc772838abddc0ce6a573725a0162bd1abd9111
athena272 <guilhermera272@gmail.com>
2024-11-16 04:51:28 -0300

    Update dbScripts.js

--- src/config/dbScripts.js
+++ src/config/dbScripts.js
@@ -1,13 +1,17 @@
 import { roomsCollection } from "./dbConnect.js"
 
-export async function findAllRooms() {
+export async function indexRooms() {
     const rooms = await roomsCollection.find().toArray()
     return rooms
 }
 
-export async function findRoom(roomName) {
+export async function showRoom(roomName) {
     const room = await roomsCollection.findOne({ name: roomName })
     return room
+}
+
+export async function storeRoom(roomName) {
+
 }
 
 export async function updateRoom({ roomName, text }) {

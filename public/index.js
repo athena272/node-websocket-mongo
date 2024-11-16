d3bcdc50393ecdab9e662c643a0769b9e90115fa
athena272 <guilhermera272@gmail.com>
2024-11-16 04:23:52 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -2,13 +2,12 @@ import './socket-front-index.js'
 
 const roomsList = document.getElementById('lista-documentos')
 
-function insertRoomLink(roomName) {
+export function insertRoomLink(roomName) {
     roomsList.innerHTML += `
      <a href="./document/document.html?roomName=${roomName}" class="list-group-item list-group-item-action">
         ${roomName}
       </a>
     `
-
 }
 
-insertRoomLink("JavaScript")
+// insertRoomLink("JavaScript")

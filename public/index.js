9cc2b7981ba7292aa1b373adcb1be35105dcbf4c
athena272 <guilhermera272@gmail.com>
2024-11-16 04:51:33 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,6 +1,14 @@
 import './socket-front-index.js'
+import { emitStoreRoom } from './socket-front-index.js'
 
 const roomsList = document.getElementById('lista-documentos')
+const form = document.getElementById('form-adiciona-documento')
+const inputForm = document.getElementById('input-documento')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  emitStoreRoom(inputForm.value)
+})
 
 export function insertRoomLink(roomName) {
     roomsList.innerHTML += `

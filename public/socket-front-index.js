ccf4750a619f32d149f1212c3662e55b7c2bbb84
athena272 <guilhermera272@gmail.com>
2024-11-16 04:51:30 -0300

    Update socket-front-index.js

--- public/socket-front-index.js
+++ public/socket-front-index.js
@@ -1,8 +1,13 @@
 import { insertRoomLink } from "./index.js"
 const socket = io()
 
-socket.emit("get_rooms", (rooms) => {
+socket.emit("index_rooms", (rooms) => {
     rooms.forEach(room => {
         insertRoomLink(room.name)
     });
 })
+
+export function emitStoreRoom(rommName) {
+    socket.emit("store_room", rommName)
+}
+

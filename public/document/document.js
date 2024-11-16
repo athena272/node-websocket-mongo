5b0cf40dc95019b1f9341f3c1c1c920e6165b80f
athena272 <guilhermera272@gmail.com>
2024-11-16 07:31:49 -0300

    Update document.js

--- public/document/document.js
+++ public/document/document.js
@@ -1,18 +1,27 @@
-import { emitTextEditor, selectRoom } from "./socket-front-document.js"
+import { emitTextEditor, showRoom, emitDeleteRoom } from "./socket-front-document.js"
 
 const urlParams = new URLSearchParams(window.location.search)
 const roomName = urlParams.get('roomName')
 const documentTitle = document.getElementById('titulo-documento')
 const textEditor = document.getElementById('editor-texto')
+const deleteButton = document.getElementById('excluir-documento')
 
 documentTitle.textContent = roomName || 'Documento sem título'
-selectRoom(roomName)
-
-export function updateTextEditor(text) {
-    textEditor.value = text
-}
+showRoom(roomName)
 
 textEditor.addEventListener('keyup', () => {
     emitTextEditor({ text: textEditor.value, roomName })
 })
 
+deleteButton.addEventListener('click', () => {
+    emitDeleteRoom(roomName)
+})
+
+export function updateTextEditor(text) {
+    textEditor.value = text
+}
+
+export function alertAndRedirect(roomName) {
+    alert(`The room with name ${roomName} was deleted!`)
+    window.location.href = '/'
+}
